388e70340f6d3c00a38031efc059ff05f1c9739b
Ivan Kraev <kukurua.tuni@gmail.com>
2024-10-17 21:36:49 +0400

    change image bars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Help from "@/components/mainPage/Help/Help";
 import HeroSection from "@/components/mainPage/HeroSection/HeroSection";
 import ImageBar from "@/components/mainPage/ImageBar/ImageBar";
 import Links from "@/components/mainPage/Links/Links";
+import LongImageBar from "@/components/mainPage/LongImageBar/LongImageBar";
 import Prices from "@/components/mainPage/Prices/Prices";
 
 export default function Home() {
@@ -13,51 +14,56 @@ export default function Home() {
     <>
       <Header />
       <HeroSection />
-      <ImageBar
+      {/* <ImageBar
         imageLinks={[
           "/img/bar/1.jpg",
           "/img/bar/6.jpg",
           "/img/bar/5.jpg",
           "/img/bar/2.jpg",
         ]}
-      />
+      /> */}
+      <LongImageBar imageLink={"/img/bar/long1.jpg"} />
       <About />
 
-      <ImageBar
+      {/* <ImageBar
         imageLinks={[
           "/img/bar/8.jpg",
           "/img/bar/9.jpg",
           "/img/bar/10.jpg",
           "/img/bar/11.jpg",
         ]}
-      />
+      /> */}
+      <LongImageBar imageLink={"/img/bar/long3.jpg"} />
       <Help />
-      <ImageBar
+      {/* <ImageBar
         imageLinks={[
           "/img/bar/12.jpg",
           "/img/bar/15.jpg",
           "/img/bar/14.jpg",
           "/img/bar/13.jpg",
         ]}
-      />
+      /> */}
+      <LongImageBar imageLink={"/img/bar/long4.jpg"} />
       <Prices />
-      <ImageBar
+      {/* <ImageBar
         imageLinks={[
           "/img/bar/16.jpg",
           "/img/bar/17.jpg",
           "/img/bar/18.jpg",
           "/img/bar/19.jpg",
         ]}
-      />
+      /> */}
+      <LongImageBar imageLink={"/img/bar/long5.jpg"} />
       <Contacts />
-      <ImageBar
+      {/* <ImageBar
         imageLinks={[
           "/img/bar/20.jpg",
           "/img/bar/24.jpg",
           "/img/bar/22.jpg",
           "/img/bar/21.jpg",
         ]}
-      />
+      /> */}
+      <LongImageBar imageLink={"/img/bar/long6.jpg"} />
       <Links />
       <Footer />
     </>
---
+++ src/components/mainPage/LongImageBar/LongImageBar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+
+export default function LongImageBar({ imageLink }: { imageLink: string }) {
+  return (
+    <div
+      className={`bg-cover bg-center flex items-center justify-around md:justify-center px-4 md:px-0 gap-4 md:gap-16 min-h-[240px] bg-green1  overflow-x-scroll md:overflow-x-hidden scrollbar-h-2 scrollbar scrollbar-thumb-green4`}
+      style={{
+        backgroundImage: `url(${imageLink})`, // Direct URL
+      }}
+    >
+      {/* {imageLinks.map((link) => (
+        <Image
+          key={link}
+          src={link}
+          alt=""
+          width={200}
+          height={200}
+          className="h-[200px] w-[200px] shadow-main"
+        />
+      ))} */}
+    </div>
+  );
+}
