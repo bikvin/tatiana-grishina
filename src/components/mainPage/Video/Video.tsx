2c2f8e9087bfc42b67ef628ad547a26e583c3c81
Ivan Kraev <kukurua.tuni@gmail.com>
2024-10-22 01:48:05 +0400

    add square decorations and animation to video

--- src/components/mainPage/Video/Video.tsx
+++ src/components/mainPage/Video/Video.tsx
@@ -1,11 +1,18 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Video() {
   return (
     <section className="py-0 md:py-20 px-8 md:px-10  overflow-x-hidden max-w-screen-lg mx-auto flex  md:flex-row flex-col-reverse mb-10">
-      <div className="flex-1 flex justify-center   items-center  ">
-        <div className="w-full md:w-[60%]">
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: "backInOut" }}
+        viewport={{ once: true, amount: 0.4 }}
+        className="flex-1 flex justify-center   items-center  relative min-h-[calc(80vw*2)] md:min-h-[calc(25vw*2)]"
+      >
+        <div className="w-[80vw] md:w-[25vw] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="relative w-full pb-[177.78%] bg-black">
             <iframe
               src={`https://player.vimeo.com/video/1021720259`}
@@ -16,8 +23,35 @@ export default function Video() {
             ></iframe>
           </div>
         </div>
-      </div>
-      <div className="flex-1 flex items-center">
+        <div
+          className="bg-green2 
+        h-[calc(1.7778*80vw*0.7)] md:h-[calc(1.7778*25vw*0.7)] 
+        w-[80vw] md:w-[25vw] 
+        absolute 
+        top-1/2 left-1/2 
+        -translate-x-[calc(80vw*0.45)] md:-translate-x-[calc(25vw*0.45)] 
+        -translate-y-[calc(80vw*0.95)] md:-translate-y-[calc(25vw*0.95)]
+        -z-10"
+        ></div>
+        <div
+          className="bg-green7 
+        h-[calc(1.7778*80vw*0.6)] md:h-[calc(1.7778*25vw*0.6)] 
+        w-[80vw] md:w-[25vw] 
+        absolute 
+        top-1/2 left-1/2 
+        -translate-x-[calc(80vw*0.55)] md:-translate-x-[calc(25vw*0.55)] 
+        -translate-y-[calc(80vw*0.3)] md:-translate-y-[calc(25vw*0.3)]
+        -z-10"
+        ></div>
+        {/* <div className="bg-green7 h-[60%] w-[40%] absolute top-1/2 left-1/2 -translate-x-[90%] -translate-y-[30%] -z-20"></div> */}
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: "backInOut" }}
+        viewport={{ once: true, amount: 0.4 }}
+        className="flex-1 flex items-center"
+      >
         <div className="hidden md:block p-8">
           <h2 className="font-bitter font-bold text-slate-800 text-4xl mb-4">
             &larr; Тут чуть подробнее. <br />
@@ -26,7 +60,7 @@ export default function Video() {
             Про мои методы и подходы.
           </p>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
