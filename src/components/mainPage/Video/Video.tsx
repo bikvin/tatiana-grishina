86019faa367a5481f82d003bbeda57659ac15e4e
Ivan Kraev <kukurua.tuni@gmail.com>
2024-10-21 23:27:18 +0400

    fix video section on mobile

--- src/components/mainPage/Video/Video.tsx
+++ src/components/mainPage/Video/Video.tsx
@@ -5,7 +5,7 @@ export default function Video() {
   return (
     <section className="py-0 md:py-20 px-8 md:px-10  overflow-x-hidden max-w-screen-lg mx-auto flex  md:flex-row flex-col-reverse mb-10">
       <div className="flex-1 flex justify-center">
-        <div className="w-[60%]">
+        <div className="w-full md:w-[60%]">
           <div className="relative w-full pb-[177.78%] bg-black">
             <iframe
               src={`https://player.vimeo.com/video/1021720259`}
@@ -18,9 +18,9 @@ export default function Video() {
         </div>
       </div>
       <div className="flex-1 flex items-center">
-        <div className="p-8">
+        <div className="hidden md:block p-8">
           <h2 className="font-bitter font-bold text-slate-800 text-4xl mb-4">
-            Тут чуть подробнее обо мне. <br />
+            &larr; Тут чуть подробнее. <br />
           </h2>
           <p>Про мои методы и подходы.</p>
         </div>
