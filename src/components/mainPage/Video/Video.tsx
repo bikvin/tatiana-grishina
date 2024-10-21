4d467cfe225d5204c61123f4d7542998db1b4af2
Ivan Kraev <kukurua.tuni@gmail.com>
2024-10-21 23:30:53 +0400

    fix video section 2

--- src/components/mainPage/Video/Video.tsx
+++ src/components/mainPage/Video/Video.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Video() {
   return (
     <section className="py-0 md:py-20 px-8 md:px-10  overflow-x-hidden max-w-screen-lg mx-auto flex  md:flex-row flex-col-reverse mb-10">
-      <div className="flex-1 flex justify-center">
+      <div className="flex-1 flex justify-center   items-center  ">
         <div className="w-full md:w-[60%]">
           <div className="relative w-full pb-[177.78%] bg-black">
             <iframe
@@ -22,7 +22,9 @@ export default function Video() {
           <h2 className="font-bitter font-bold text-slate-800 text-4xl mb-4">
             &larr; Тут чуть подробнее. <br />
           </h2>
-          <p>Про мои методы и подходы.</p>
+          <p className="font-thin  text-lg tracking-wide">
+            Про мои методы и подходы.
+          </p>
         </div>
       </div>
     </section>
